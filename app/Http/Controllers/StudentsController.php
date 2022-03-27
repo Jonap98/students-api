d32ccbf81713801c087339e7f98f129e82f9566f
jona <jona_jko@hotmail.com>
2022-03-27 17:27:38 -0600

    Agrega getUsuarios api

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Students;
 
 class StudentsController extends Controller
 {
-    //
+    public function getStudents() {
+        $students = Students::all();
+
+        return response()->json([
+            'result' => true,
+            'datos' => $students
+        ]);
+    }
 }
